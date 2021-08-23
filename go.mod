be3361542b569947578986e57359d1f1552a27f3
lucianopeixoto <lucianopeixoto@hotmail.com>
2021-08-23 14:25:46 -0300

    Initial Commit. Code does nothing yet.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module lucianocpeixoto/mg1-to-nt
+
+go 1.17
---
+++ mg1-to-nt.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Minew G1 adapter for nTopus Indoor Location.")
+	fmt.Println("v 0.00")
+	fmt.Println("Starting App...")
+}
